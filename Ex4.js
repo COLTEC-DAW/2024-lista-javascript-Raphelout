81730ff8ff88de737d57e2e0b43326f0a95618b3
Raphelout <145383096+Raphelout@users.noreply.github.com>
2024-06-18 17:35:10 -0300

    Ex4

---
+++ Ex4.js
@@ -0,0 +1,13 @@
+for(let i = 1; i<=100; i++){
+    if(i%5==0 || i%3==0){
+        if(i%5==0 && i%3==0){
+            console.log("FizzBuzz");
+        }else if(i%3==0){
+            console.log("Fizz");
+        } else{
+            console.log("Buzz");
+        }
+    }else{
+        console.log(i);
+    }
+}
