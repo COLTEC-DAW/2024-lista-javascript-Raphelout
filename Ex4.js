8761dc76a9e3828fc6c527f42e9b1140d09df350
Raphelout <145383096+Raphelout@users.noreply.github.com>
2024-06-18 18:15:41 -0300

    Ex6, Ex4 update

--- Ex4.js
+++ Ex4.js
@@ -1,8 +1,8 @@
 for(let i = 1; i<=100; i++){
-    if(i%5==0 || i%3==0){
-        if(i%5==0 && i%3==0){
+    if(i%5===0 || i%3===0){
+        if(i%5===0 && i%3===0){
             console.log("FizzBuzz");
-        }else if(i%3==0){
+        }else if(i%3===0){
             console.log("Fizz");
         } else{
             console.log("Buzz");
